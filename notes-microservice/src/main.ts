1fbc9bd22ba73715c2a8830edd7713ff61cbb595
Diego Flores <u20221b394@upc.edu.pe>
2025-04-10 19:21:53 -0500

    changes

--- notes-microservice/src/main.ts
+++ notes-microservice/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { environmnets } from './config/environments';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+async function main() {
+  const logger = new Logger('Notes-Microservice');
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.NATS,
+    options: {
+      servers: environmnets.natsServer
+    }
+  });
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+  }))
+  await app.listen();
+  logger.log(`Notes microservice listen on ${environmnets.natsServer}`);
 }
-bootstrap();
+main();
